64c0d957ae5238f443ce12bc9ebfd2bf2915f34c
dev-faya <toumi7366@gmail.com>
2018-11-14 11:24:07 +0000

    Updates

---
+++ precache-manifest.d230ca0cb93a857f31ef05476e2fbeaa.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "86a5ee86cdf4b19bef0de52c3c129874",
+    "url": "/x-portfolio/static/media/03.86a5ee86.jpg"
+  },
+  {
+    "revision": "0c37a33626f3e5c5ac78",
+    "url": "/x-portfolio/static/css/main.9cfcdf0d.chunk.css"
+  },
+  {
+    "revision": "1e9492df0be3a7d28445",
+    "url": "/x-portfolio/static/js/1.1e9492df.chunk.js"
+  },
+  {
+    "revision": "5f686f772a078628e71d",
+    "url": "/x-portfolio/static/js/runtime~main.5f686f77.js"
+  },
+  {
+    "revision": "3e1862ed54418f28df04cdbfbfae4394",
+    "url": "/x-portfolio/static/media/avatar.3e1862ed.jpeg"
+  },
+  {
+    "revision": "24ed36dcceaf650fa7ae95fc0f4adb17",
+    "url": "/x-portfolio/static/media/01.24ed36dc.jpg"
+  },
+  {
+    "revision": "19ee6b731a23379826be1d1c9ea9621b",
+    "url": "/x-portfolio/static/media/02.19ee6b73.jpg"
+  },
+  {
+    "revision": "0c37a33626f3e5c5ac78",
+    "url": "/x-portfolio/static/js/main.0c37a336.chunk.js"
+  },
+  {
+    "revision": "be722c92284e66e3fc27316b630aae99",
+    "url": "/x-portfolio/static/media/mobirise-icons.be722c92.eot"
+  },
+  {
+    "revision": "efe575ff53d0d0e86d5f5fc36f574e2b",
+    "url": "/x-portfolio/static/media/mobirise-icons.efe575ff.ttf"
+  },
+  {
+    "revision": "527def02d22e0e4c65242eee800cbaf7",
+    "url": "/x-portfolio/static/media/mobirise-icons.527def02.woff"
+  },
+  {
+    "revision": "629e694a519d9cb80f7502d46bf62fdf",
+    "url": "/x-portfolio/static/media/mobirise-icons.629e694a.svg"
+  },
+  {
+    "revision": "0ec8beacbcd236e827c405e9625318a4",
+    "url": "/x-portfolio/static/media/office.0ec8beac.jpg"
+  },
+  {
+    "revision": "5ce5614795a13faa1d4869891c3f9a7a",
+    "url": "/x-portfolio/static/media/background.5ce56147.jpg"
+  },
+  {
+    "revision": "16273c07086bd342e96efe85cdd3bb34",
+    "url": "/x-portfolio/index.html"
+  }
+];
